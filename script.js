4c10b61fed601e9d51cf106490d544def7016126
JoshuaLucas212 <Joshluke342@gmail.com>
2022-12-05 18:40:12 -0700

    API for dropdown menu (not done with code yet, just committing so i can pull

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+var DropButton = document.getElementById('drop-box');
 
-getTeamsApi();
-getScheduleApi(29);
+function DropButton() {
+    fetch('https://www.balldontlie.io/api/v1/teams')
+    .then((response) => response.json())
+    .then((result) => {
+        result.data.forEach(team => {
+            for (let name in team) {
+                if (team.name =="Jazz"){
+                    console.log(team.id, team.name)
+                }
+            }
+            
+        });
+    })
+}
+
+//DropButton.addEventListener('click')

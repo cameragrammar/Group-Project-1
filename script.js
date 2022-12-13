e657e8beb082259780cd121a9296707adb580488
Joseph Hayes <joey.hayes.1996@gmail.com>
2022-12-12 17:41:47 -0700

    Finished the js, css, html, fixed bugs, refactored code

--- script.js
+++ script.js
@@ -1,32 +1,46 @@
+//Selectors
+const playerSearch = document.querySelector('#drop-box-player input');
+const searchButton = document.getElementById('searchbutton');
+const saveCheckbox = document.getElementById("save-checkbox");
+const savedPlayers = document.getElementById("saved-players");
+const searchResultBox = document.getElementById("search-container");
+const clearButton = document.getElementById("clear-games");
+const results = document.querySelector('#search-results');
+const teamDiv = document.querySelector('#team');
+const stats = document.querySelector('#player-stats');
+const schedule = document.querySelector("#schedule");
 
-var PlayerSearch = document.querySelector('#drop-box-player input').value;
-var SearchButton = document.getElementById('searchbutton');
-const PlayerApi = "https://www.balldontlie.io/api/v1/players";
-const searchResultBox = document.getElementById("search-results");
-var SavePlayer = document.getElementById("Player-stats");
-var SaveTeam = document.getElementById("schedule");
+//Variables
 let playerSearchResults = [];
-var team;
-let playerResultsArr = JSON.parse(localStorage.getItem("savedPlayers")) || [];
-let playerInfo;
-let saveCheckbox = document.getElementById("save-checkbox");
-let savedPlayers = document.getElementById("saved-players");
-console.log(saveCheckbox)
+let team;
+let selectedPlayerId;
+let selectedPlayerTeam;
+let selectedPlayerInfo;
+let teamArr = [];
+let eventList;
 
-//Adds API into SearchButton as well as pulling the search results and adding it into the button.
+//localStorage
+let playerResultsArr = JSON.parse(localStorage.getItem("savedPlayers")) ?? [];
+
+//API Links
+const ticketApi = "https://app.ticketmaster.com/discovery/v2/events?";
+const ticketKey = "apikey=YUtofAEqmAbWVA8ezVcGnMFbUZoJeDvh";
+const playerApi = "https://www.balldontlie.io/api/v1/players";
+
+//Functions
+//Search funtion
 function getPlayersApi(playerName) {
     let playerObj;
     let playerData;
     playerSearchResults = [];
-    fetch(PlayerApi + "?per_page=100&search=" + playerName)
+    fetch(playerApi + "?per_page=100&search=" + playerName)
     .then((response) => response.json())
     .then((result) => {
         playerData = result.data
-        console.log(result.data)
 
         //Hans Code to display search results
-        var results = document.querySelector('#search-results');
-        var output = '';
+
+        let output = results.innerHTML;
 
         playerData.forEach(player => {
             playerObj = {};
@@ -34,43 +48,25 @@ function getPlayersApi(playerName) {
             playerObj.playerName = player.first_name + " " + player.last_name;
             playerObj.teamName = player.team.name;
             playerObj.pos = player.position;
-            console.table(playerObj)
             playerSearchResults.push(playerObj);
-            let playerFullName = par
-            // below causing issues with displaying results as not every player has a position. Maybe this was to find active players only?
             if (player.position != "") {
                 team = player.team.id;
-                //  output += "<p id = '" + player.id + "' onclick='displayStats()'"+ " team='" + playerObj.teamName + "'>" + playerObj.playerName + " | " + playerObj.teamName + " </p>";
-                output += `<p id =${player.id} onclick=displayStats("${player.last_name}") team=${playerObj.teamName}>${playerObj.playerName} | ${playerObj.teamName}</p>`;
+                output += `<p id =${player.id} onclick='displayStats("${player.first_name + " " + player.last_name}")
+                ' team=${playerObj.teamName}'>${playerObj.playerName} | ${playerObj.teamName}</p>`;
             }
         })
-
-       //Hans Code to innerHTML display
         results.innerHTML = output;
-        console.log(playerSearchResults);
-        
-        //display all results of the search - should go last
         searchResultBox.style.display = "block";
     })
 }
 
-SearchButton.addEventListener('click', function() {
-    var PlayerSearch = document.querySelector('#drop-box-player input').value;
-    getPlayersApi(PlayerSearch);
-})
-
-//Hans Code to Display Stats in player-stats box
+//Display Stats function
 function displayStats(fullName) {
-    var id = event.target.id;
-    playerInfo = fullName;
-    console.log(playerInfo)
-    console.log(id);
-    console.log(event.target);  
-    fetch('https://www.balldontlie.io/api/v1/season_averages?player_ids[]=' + id)
+    selectedPlayerId = event.target.id;
+    playerName = fullName;
+    fetch('https://www.balldontlie.io/api/v1/season_averages?player_ids[]=' + selectedPlayerId)
     .then((response) => response.json())
     .then((result) => {
-        console.log(result);
-        var stats = document.querySelector('#player-stats');
         let output = '';
         if ( result.data.length >= 1) {
         //output += "<p id='team'> Team: " + team + "</p>";
@@ -79,132 +75,84 @@ function displayStats(fullName) {
         output += "<p> Assists: " + result.data[0].ast + "</p>";
         output += "<p> Steals: " + result.data[0].stl + "</p>";
         output += "<p> Blocks: " + result.data[0].blk + "</p>";
-
-        //getScheduleApi(team);
         } else {
             output = "<p> No stats to display for the current season </p>"
         }
         stats.innerHTML = output;
     })
-    fetch('https://www.balldontlie.io/api/v1/players/' + id)
+    fetch('https://www.balldontlie.io/api/v1/players/' + selectedPlayerId)
     .then((response) => response.json())
     .then((result) => {
-        console.log("team");
-        console.log(result.team.id);
-        var stats = document.querySelector('#team');
+        selectedPlayerInfo = {};
+        selectedPlayerInfo.id = result.id;
+        selectedPlayerInfo.name = fullName;
+        selectedPlayerInfo.team = result.team.name;
         let output = '';
         output += " Team: " + result.team.abbreviation;
-        stats.innerHTML = output;
+        teamDiv.innerHTML = output;
+        saveCheckbox.style = "display: flex"
         getTicketApi(result.team.name);
     })
 }
 
-//API Links
-const ballApi = "https://www.balldontlie.io/api/v1/";
-const ticketApi = "https://app.ticketmaster.com/discovery/v2/events?";
-const ticketKey = "apikey=YUtofAEqmAbWVA8ezVcGnMFbUZoJeDvh";
-
-//Get Current Date for API > Games Schedule query
-const today = new Date();
-const dateStr = today.getFullYear() + "-" + String(today.getMonth() + 1).padStart(2, '0') + "-" + String(today.getDate()).padStart(2, '0');
-
-//Array to hold all teams and their IDs from the API. This is currently being console logged
-const teamArr = [];
-
-let eventList;
-const eventOutput = [];
-
-//get all teams, and build the array of teams/ids. This creates an array of objects for reference
-//just need to push this result to the dropdown menu(s)
-function getTeamsApi(){
-    fetch("https://www.balldontlie.io/api/v1/teams")
-    .then((response) => response.json())
-    .then((result) => {
-        console.log(result.data);
-        let teamObj = {};
-        result.data.forEach(team => {
-                if (!teamArr.includes(team.name)) {
-                    teamObj = {};
-                    teamObj.id = team.id;
-                    teamObj.name = team.name;
-                    teamArr.push(teamObj);
-                }
-        })
-    })
-}
-
-//This function gets the entire upcoming schedule for a certain team, and organizes the array of objects
-//by date, ascending (shortest # days to the game is first on the list)
-function getScheduleApi(teamId) {
-    fetch(ballApi + "games?per_page[]=100&start_date=" + dateStr + "&team_ids[]=" + teamId)
-    .then((response) => response.json())
-    .then((result) => 
-    {
-        result.data.sort(function(a, b) {
-            return new Date(a.date) - new Date(b.date);
-          });
-        console.log(result.data);
-        displaySchedule(result.data);
-    })
-}
-
-//can search by team name, in the event search api
+//Get Ticketmaster Games
 function getTicketApi(teamName) {
     fetch(ticketApi + ticketKey + "&keyword=" + teamName + "&promoterId=695&size=10")
     .then((response) => response.json())
     .then((result) => {
-        console.log(result)
         eventList = result._embedded.events;
         eventList.sort(function(a, b) {
             return new Date(a.dates.start.localDate) - new Date(b.dates.start.localDate);
         });
-        // eventList.forEach(event => {
-        //     console.log(event.name, event.dates.start.localDate, event.url);
-        // })
         displaySchedule(eventList);
     })
 }
 
+//Display Ticketmaster Data
 function displaySchedule(data) {
-    var schedule = document.querySelector("#schedule");
     let output = "";
     data.forEach(game => {
         output += "<p><a href='" + game.url + "'>" + game.dates.start.localDate + " " + game.name + "</a></p>";
     })
     schedule.innerHTML = output;
-
-    function SavePlayer(PlayerButton) {
-        const jsonObj = JSON.stringify(PlayerButton);
-        localStorage.setItem("PlayerButton", jsonObj);
-        const str = localStorage.getItem("PlayerButton");
-        const parsedObj = JSON.parse(str);
-        console.log(parsedObj);
-    }
-
-SavePlayer.addEventListener("click", function() {
-getPlayersApi(data);
-})
-
 }
 
-let playerTeamId;
-let playerId;
-
-saveCheckbox.addEventListener("click", function() {
-        playerResultsArr.push(playerInfo);
-        localStorage.setItem("savedPlayers", JSON.stringify(playerResultsArr));
-})
-
+//Display saved players
 function displaySavedPlayers() {
+    playerResultsArr = JSON.parse(localStorage.getItem("savedPlayers")) ?? [];
     let output = "";
-    let savedPlayersDB = JSON.parse(localStorage.getItem("savedPlayers"));
-    if (savedPlayersDB == null) {
-        savedPlayersDB = [];
-    }
-    savedPlayersDB.forEach(player => {
-        output += "<p>" + player + "</p>";
+    playerResultsArr.forEach(player => {
+        output += `<p id=${player.id} onclick="displayStats('${player.name}')" team=${player.team}> ${player.name} | ${player.team} </p>`;
     })
     savedPlayers.innerHTML = output;
 }
 
+//EventListeners
+searchButton.addEventListener('click', function() {
+    getPlayersApi(playerSearch.value);
+})
+
+clearButton.addEventListener("click", () => {
+    localStorage.clear();
+    savedPlayers.innerHTML = "";
+    displaySavedPlayers();
+})
+
+saveCheckbox.addEventListener("click", function () {
+  let exist = false;
+  if (playerResultsArr.length >= 1) {
+    playerResultsArr.forEach((player) => {
+      if (player.id === selectedPlayerInfo.id) {
+        exist = true;
+      }
+    });
+  }
+  if (selectedPlayerInfo && !exist) {
+    playerResultsArr.push(selectedPlayerInfo);
+    localStorage.setItem("savedPlayers", JSON.stringify(playerResultsArr));
+  }
+  displaySavedPlayers();
+});
+
+//main
 displaySavedPlayers();
